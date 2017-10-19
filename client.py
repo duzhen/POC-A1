efe7b451b0bf53b30fa3e32d2dd631a6498a0d52
Zhen Du <jenkin.du@gmail.com>
2017-10-19 12:13:43 -0400

    first commit, finish get post put delete

---
+++ client.py
@@ -0,0 +1,78 @@
+import requests
+import rfq_pb2
+import argparse
+import time
+from random import randint
+
+url = 'http://127.0.0.1:5002/rfq'
+
+def PostRequest(Request, id=int(time.time())):
+    Request.id = id
+    Request.account = 'post new account'
+    Request.number = randint(1, 100)
+    Request.category = 'POST NEW'
+    Request.quantity = randint(1, 10)
+
+def PutRequest(Request, id=int(time.time())):
+    Request.id = id
+    Request.account = 'put new account'
+    Request.number = randint(1, 100)
+    Request.category = 'PUT NEW'
+    Request.quantity = randint(1, 10)
+
+def DelRequest(Request, id=1):
+    Request.id = id
+    #Request.account = 'apple'
+    #Request.number = randint(0, 100)
+    #Request.category = 'C'
+    #Request.quantity = randint(0, 100)
+
+parser = argparse.ArgumentParser(description="Protocol Buffer Client.")
+subparsers = parser.add_subparsers(help='commands')
+get_parser = subparsers.add_parser('get', help='executes a HTTP GET request and prints the response.')
+get_parser.add_argument("ID", type=int, nargs='?', help="Request ID")
+get_parser.set_defaults(which='get')
+
+post_parser = subparsers.add_parser('post', help='executes a HTTP POST request and prints the response.')
+post_parser.add_argument("ID", type=int, nargs='?', help="Request ID")
+post_parser.set_defaults(which='post')
+
+put_parser = subparsers.add_parser('put', help='executes a HTTP PUT request and prints the response.')
+put_parser.add_argument("ID", type=int, nargs='?', help="Request ID")
+put_parser.set_defaults(which='put')
+
+del_parser = subparsers.add_parser('delete', help='executes a HTTP DELETE request and prints the response.')
+del_parser.add_argument("ID", type=int, nargs='?', help="Request ID")
+del_parser.set_defaults(which='delete')
+
+args = parser.parse_args()
+
+rfq = rfq_pb2.Request()
+
+if args.which == 'get':
+    if args.ID:
+        response = requests.get(url+'/'+str(args.ID))
+    else:
+        response = requests.get(url)
+elif args.which == 'post':
+    if args.ID:
+        PostRequest(rfq, args.ID)
+    else:
+        PostRequest(rfq)
+    response = requests.post(url, data=rfq.SerializeToString(), headers={'Content-Type': 'application/octet-stream'})
+elif args.which == 'put':
+    if args.ID:
+        PutRequest(rfq, args.ID)
+    else:
+        PutRequest(rfq)
+    response = requests.put(url, data=rfq.SerializeToString(), headers={'Content-Type': 'application/octet-stream'})
+elif args.which == 'delete':
+    if args.ID:
+        DelRequest(rfq, args.ID)
+    else:
+        DelRequest(rfq)
+    response = requests.delete(url, data=rfq.SerializeToString(), headers={'Content-Type': 'application/octet-stream'})
+print("Protocol Buffer Value:")
+print(rfq.SerializeToString())
+print("Response Json Value:")
+print(response.content)
---
+++ server.py
@@ -0,0 +1,150 @@
+
+#build a running REST API using
+# python
+#Flask web framework 
+#Flask Restful extension
+
+# prepare the data
+
+# $ virtualenv venv
+# $ source venv/bin/activate
+# $ pip install flask flask-jsonpify flask-sqlalchemy flask-restful
+# $ pip freeze 
+
+# run this server.py code
+
+# http://127.0.0.1:5002/employees 
+# http://127.0.0.1:5002/tracks
+# http://127.0.0.1:5002/employees/8
+
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+# from json import dumps
+from flask.ext.jsonpify import jsonify
+from flask import abort
+
+import rfq_pb2
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+database = [
+    {
+        'id': 1,
+        'account': 'apple',
+        'number': 100,
+        'category': 'A',
+        'quantity': 1000
+    },
+    {
+        'id': 2,
+        'account': 'linux',
+        'number': 200,
+        'category': 'B',
+        'quantity': 2000
+    },
+    {
+        'id': 3,
+        'account': 'windows',
+        'number': 300,
+        'category': 'C',
+        'quantity': 3000
+    },
+    {
+        'id': 4,
+        'account': 'ios',
+        'number': 400,
+        'category': 'D',
+        'quantity': 4000
+    }
+]
+
+@app.route('/')
+def index():
+    return "Assignment 1<p>" \
+           "/rfq    show all the data<p>" \
+           "/rfq/id get specific quote"
+
+class rfq(Resource):
+    def get(self):
+        return jsonify(database)
+    def post(self):
+        rfq = rfq_pb2.Request()
+        print("Post Protocol Buffer Request:")
+        print(request.data)
+        rfq.ParseFromString(request.data)
+
+        data = [d for d in database if d['id'] == rfq.id]
+        if len(data) != 0:
+            abort(400)
+
+        d = {
+            'id': rfq.id,
+            'account': rfq.account,
+            'number': rfq.number,
+            'category': rfq.category,
+            'quantity': rfq.quantity
+        }
+        database.append(d)
+
+        return jsonify({'price': d['number'] + d['quantity'],
+                        'period': [time.asctime(time.localtime(abs(d['id'] - 24 * 3600))),
+                                   time.asctime(time.localtime(abs(d['id'])))]})
+
+    def put(self):
+        rfq = rfq_pb2.Request()
+        print("Put Protocol Buffer Request:")
+        print(request.data)
+        rfq.ParseFromString(request.data)
+
+        data = [d for d in database if d['id'] == rfq.id]
+        if len(data) == 0:
+            abort(400)
+
+        d = data[0]
+        d['account'] = rfq.account
+        d['number'] = rfq.number
+        d['category'] = rfq.category
+        d['quantity'] = rfq.quantity
+
+        return jsonify({'price': d['number'] + d['quantity'],
+                        'period': [time.asctime(time.localtime(abs(d['id'] - 24 * 3600))),
+                                   time.asctime(time.localtime(abs(d['id'])))]})
+
+    def delete(self):
+        rfq = rfq_pb2.Request()
+        print("Delete Protocol Buffer Request:")
+        print(request.data)
+        rfq.ParseFromString(request.data)
+
+        data = [d for d in database if d['id'] == rfq.id]
+        if len(data) == 0:
+            abort(400)
+        print("Delete ID:"+str(rfq.id))
+
+        d = data[0]
+        database.remove(data[0])
+
+        return jsonify({'price': d['number'] + d['quantity'],
+                        'period': [time.asctime(time.localtime(abs(d['id'] - 24 * 3600))),
+                                   time.asctime(time.localtime(abs(d['id'])))]})
+
+class rfq_id(Resource):
+    def get(self, id):
+        data = [d for d in database if d['id'] == id]
+        if len(data) == 0:
+            abort(404)
+        print("Get ID:" + str(id))
+        d = data[0]
+        return jsonify({'price': d['number'] + d['quantity'],
+                        'period': [time.asctime(time.localtime(abs(d['id'] - 24 * 3600))),
+                                   time.asctime(time.localtime(abs(d['id'])))]})
+
+api.add_resource(rfq, '/rfq')
+api.add_resource(rfq_id, '/rfq/<int:id>')
+
+if __name__ == '__main__':
+     app.run(port='5002', threaded=True)
